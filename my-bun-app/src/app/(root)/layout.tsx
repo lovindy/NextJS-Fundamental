c6970e47031b0c0c582163e838e9be73c21df1e0
lovindy <seangdy085@gmail.com>
2024-11-02 21:58:55 -0700

    auth : implement login and logout with github

--- my-bun-app/src/app/(root)/layout.tsx
+++ my-bun-app/src/app/(root)/layout.tsx
@@ -1,17 +1,14 @@
-import React from "react";
+import NavBar from "../../components/NavBar";
 
-const layout = ({ children }: { children: React.ReactNode }) => {
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <div>
-      <header>
-        <h1>Navigate Bar</h1>
-      </header>
-      <main>{children}</main>
-      <footer>
-        <p>�� 2022 My App. All rights reserved.</p>
-      </footer>
-    </div>
+    <main className="font-work-sans">
+      <NavBar />
+      {children}
+    </main>
   );
-};
-
-export default layout;
+}
--- my-bun-app/src/app/(root)/page.tsx
+++ my-bun-app/src/app/(root)/page.tsx
@@ -1,8 +1,6 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="">
+    <div className="text-2xl">
       <p>Home Page</p>
     </div>
   );
